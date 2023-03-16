2aca43978a00c81a2b7cd26b6a096c63aa933bfa
Emre Dogan <emre.dogan@shortcut.no>
2023-03-16 14:34:15 +0100

    Initial Commit

---
+++ Football-API-2023/Football_API_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Football_API_2023App.swift
+//  Football-API-2023
+//
+//  Created by Emre Dogan on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Football_API_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
